2eb05e2b1edd79f1319691675897224b3b7793e5
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2026-02-21 20:58:40 -0500

    creacion del objeto que contiene tipo, tamano, cantidad, precio por unidad, total a pagar, numero_pedido, estado pedido

---
+++ ObjFastFood.py
@@ -0,0 +1,11 @@
+class ObjFastFood:
+    def __init__ (self, tipo=0, tamano=0, cantidad=0, precio_unidad=0.0,
+                 total_pagar=0.0, numero_pedido=0, estado_pedido=0):
+        self.tipo = tipo
+        self.tamano = tamano
+        self.cantidad = cantidad
+        self.precio_unidad = precio_unidad
+        self.total_pagar = total_pagar
+        self.numero_pedido = numero_pedido
+        self.estado_pedido = estado_pedido
+        
