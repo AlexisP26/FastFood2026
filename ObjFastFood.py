0ca95a4186c303b4d2879ab34ffc1147197cf153
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2026-02-23 13:16:54 -0500

    cambio en clase, ahora tendremos clase pedido y clase gestor de pedidos, añadimos tambien un archivo para definir el preoblema, con sur requerimientos funcionales y no funcionales, las entidades del sistema y las reglas de negocio

--- ObjFastFood.py
+++ ObjFastFood.py
@@ -1,11 +1,18 @@
-class ObjFastFood:
-    def __init__ (self, tipo=0, tamano=0, cantidad=0, precio_unidad=0.0,
-                 total_pagar=0.0, numero_pedido=0, estado_pedido=0):
+class Pedido:
+
+    Estado_Pendiente = 0
+    Estado_Atendido = 1
+
+    def __init__(self, numero, tipo, tamano, cantidad, precio_unitario):
+        self.numero = numero
         self.tipo = tipo
         self.tamano = tamano
         self.cantidad = cantidad
-        self.precio_unidad = precio_unidad
-        self.total_pagar = total_pagar
-        self.numero_pedido = numero_pedido
-        self.estado_pedido = estado_pedido
-        
+        self.precio_unitario = precio_unitario
+        self.estado = Pedido.Estado_Pendiente
+
+    def calcular_total(self):
+        return self.cantidad * self.precio_unitario
+
+    def cambiar_estado(self):
+        self.estado = Pedido.Estado_Atendido
